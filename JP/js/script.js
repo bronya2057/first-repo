985b2a145bc6f7afefb8128b236a7e3b4bfd371d
Alex Brodskyi <bronya10@gmail.com>
2016-06-28 20:11:28 +0300

    Added Javascript [Form name][Element name].value to a variable

--- JP/js/script.js
+++ JP/js/script.js
@@ -1,8 +1,19 @@
 /*global alert*/
 /*global console*/
+/*global document*/
 
 /*alert("hello");
 console.log("hello");
 document.write("what ta");
 var cars=[1,2,3];*/
-
+function submitAnswers()
+{
+    var total=5;
+    var score=0;
+    //get user input;
+    var q1=document.forms.quizForm.q1.value;
+    var q2=document.forms.quizForm.q2.value;
+    var q3=document.forms.quizForm.q3.value;
+    var q4=document.forms.quizForm.q4.value;
+    var q5=document.forms.quizForm.q5.value;
+}
